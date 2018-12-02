5688dd47bb9ad8e431ff841666ec0f2d7bc99edd
kouts34 <36401991+kouts34@users.noreply.github.com>
2018-12-02 17:21:58 +0200

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,1 +1,13 @@
-<?php echo "test"; exit; ?>
+<?php 
+	
+	//If there is now constant defined called __CONFIG__, do not load this file
+	if(!defined('__CONFIG__')) {
+		exit('You dont have a config file'); 
+	}
+
+
+	//Config below
+
+
+
+?> 
