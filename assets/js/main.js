258d19e7b1c529a447befb5ccf4c166c2ebf00d3
kouts34 <36401991+kouts34@users.noreply.github.com>
2019-03-14 19:58:41 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,18 @@ $(document)
 	event.preventDefault();
 
 	var _form = $(this);
+	var _error = $(".js-error", _form);
 
 	var data = {
 		email: $("input[type='email']", _form).val(),
 		password: $("input[type='password']", _form).val()
+	};
+
+	if(data.email.length < 6) {
+		_error
+			.text("Please enter a valid email address")
+			.show();
+		return false;
 	}
 
 	console.log(data);
