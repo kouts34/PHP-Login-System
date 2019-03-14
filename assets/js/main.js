c5410564bfb128d2d01aa785bfe66df50a530c6c
kouts34 <36401991+kouts34@users.noreply.github.com>
2019-03-14 19:40:51 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
-alert('test');
+$(document)
+.on("submit", "form.js-register", function(event) {
+	event.preventDefault();
+
+	var _form = $(this);
+
+	var date = {
+		email: $("input[type='email']", _form).val(),
+		password: $("input[type='password']", _form).val()
+	}
+
+	console.log(data);
+
+	return false;
+})
